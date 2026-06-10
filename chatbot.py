531ac4c4e587cdc745ff3fa2ae7368a2896a8853
Inception72 <ianmckisson@gmail.com>
2026-06-10 12:27:03 -0400

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -9,7 +9,7 @@ if "messages" not in st.session_state:
   st.session_state.message = []
 
 #Display caht messages from history on app rerun 
-for message in st.session state.messages:
+for message in st.session_state.messages:
   with st.chat_message(message["role"]):
     st.markdown(message["content"])
 
